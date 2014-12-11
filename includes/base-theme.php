2ed3057d81c59480521aa460587a6bb5c2a24cd2
Eric <eric.bright@designshopp.com>
2014-12-11 13:16:17 -0500

    look at that, a typo

--- includes/base-theme.php
+++ includes/base-theme.php
@@ -199,7 +199,7 @@ class navWalker extends Walker_Nav_Menu{
 		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
 
 		$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) );
-		$class_names = ' class="' . esc_attr( $class_names ) .  'post-id-'.esc_attr( $item->object_id ).'"';
+		$class_names = ' class="' . esc_attr( $class_names ) .  ' post-id-'.esc_attr( $item->object_id ).'"';
 
 		//$output .= $indent . '<li id="menu-item-'. $item->ID . '"' . $value . $class_names .'>';
 		$output .= $indent . '<li' . $value . $class_names .'>';
