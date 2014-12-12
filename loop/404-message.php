243708e6f387fade149cc5c9dd9234aeadfb16ba
Eric <eric.bright@designshopp.com>
2014-12-12 16:48:21 -0500

    oocss

--- loop/404-message.php
+++ loop/404-message.php
@@ -1,4 +1,4 @@
-<?php if (get_lang_active() == 'fr') { ?>
+<?php if (get_lang_active('fr')) { ?>
 	Désolé, nous n'avons pas pu trouver la page que vous cherchiez.
 <?php } else { ?> <!-- English Content -->
 	Sorry, but you've found a page that does not exist, please use the above navigation to find your way back.
