ef550eed44921b92c0fc8745c28430a724333034
ericbdev <ericbdev@gmail.com>
2015-02-10 10:33:28 -0500

    added custom post type, forked from other githubber jjgrainger. added in php fall back for array_replace_recursive

--- includes/base-theme.php
+++ includes/base-theme.php
@@ -32,6 +32,11 @@ function cmb_initialize_cmb_meta_boxes() {
 }
 add_action( 'init', 'cmb_initialize_cmb_meta_boxes', 9999 );
 
+if (!class_exists( 'customPostType' ) ){
+	require_once 'vendor/custom-post-types.php';
+}
+
+
 require_once 'base/archive-helpers.php';
 require_once 'base/disable-feeds.php';
 require_once 'base/image.php';
@@ -308,6 +313,47 @@ if (!function_exists('array_replace')){
 			endif;
 		}
 
+		return $array;
+	}
+}
+if (!function_exists('array_replace_recursive'))
+{
+	function array_replace_recursive($array, $array1)
+	{
+		function recurse($array, $array1)
+		{
+			foreach ($array1 as $key => $value)
+			{
+				// create new key in $array, if it is empty or not an array
+				if (!isset($array[$key]) || (isset($array[$key]) && !is_array($array[$key])))
+				{
+					$array[$key] = array();
+				}
+
+				// overwrite the value in the base array
+				if (is_array($value))
+				{
+					$value = recurse($array[$key], $value);
+				}
+				$array[$key] = $value;
+			}
+			return $array;
+		}
+
+		// handle the arguments, merge one by one
+		$args = func_get_args();
+		$array = $args[0];
+		if (!is_array($array))
+		{
+			return $array;
+		}
+		for ($i = 1; $i < count($args); $i++)
+		{
+			if (is_array($args[$i]))
+			{
+				$array = recurse($array, $args[$i]);
+			}
+		}
 		return $array;
 	}
 }
--- includes/base/post-types.php
+++ includes/base/post-types.php
@@ -1,43 +1,6 @@
 <?php
-function customPostTypes() {
-	/*register_post_type('type-story', array(
-		'labels'             => array(
-			'name'          =>  _x( 'Success Stories', 'Titles', themeDomain() ),
-			'singular_name' => _x( 'Success Story', 'Titles', themeDomain() ),
-			'menu_name'     => _x( 'Success Stories', 'Titles', themeDomain() ),
-		),
-		'public'             => false,
-		'publicly_queryable' => true,
-		'show_ui'            => true,
-		'show_in_menu'       => true,
-		'query_var'          => true,
-		'rewrite'             => array('slug' => _x('success-stories', 'URL Slug', themeDomain()), 'with_front' => false, 'page' => false),
-		'capability_type'    => 'page',
-		'has_archive'        => true,
-		'hierarchical'       => false,
-		'menu_position'      => 20,
-		//'menu_icon'          => 'dashicons-list-view',
-		'supports'           => array('title','custom-fields', 'revisions')
-	));*/
-
-	$catArgs = 		array(
-		'label'                 => __('Category', themeDomain()),
-
-		'sort'                  => true,
-		'show_ui'               => true,
-		'show_admin_column'     => true,
-		'hierarchical'          => true,
-		'update_count_callback' => '_update_post_term_count',
-		'query_var'             => true,
-		'args'                  => array('orderby' => 'term_order'),
-	);
-	//register_taxonomy(	'stories', array('type-story'), $catArgs);
-	//register_taxonomy(	'articles', array('type-article'), $catArgs);
-
-
-}
-//add_action('init', 'customPostTypes');
-
+$supports = array('title','custom-fields', 'revisions');
+//$generic = new customPostType('generic', $supports);
 
 /** Use this to deregister bad post types **/
 if (!function_exists('unregister_post_type')) :
