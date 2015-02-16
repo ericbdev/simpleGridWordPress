b5d366b6c5a85a2cf91ebbe11cf35e7dc95d1163
ericbdev <ericbdev@gmail.com>
2015-02-16 14:50:42 -0500

    because trailing slashes in variables are confusing, and variables in double quotes work

--- functions.php
+++ functions.php
@@ -24,29 +24,29 @@ function get_the_prefix() {
 }
 
 function load_my_scripts() {
-	$cssPath = get_template_directory_uri().'/css/';
-	$jsPath = get_template_directory_uri().'/scripts/';
+	$cssPath = get_template_directory_uri().'/css';
+	$jsPath = get_template_directory_uri().'/scripts';
 
 	if (!is_admin()) {  //If the page is admin page, don't load//
-		wp_enqueue_script( 'modernizr', $jsPath.'vendor/modernizr.js', false, '1.0', false);
+		wp_enqueue_script( 'modernizr', "$jsPath/vendor/modernizr.js", false, '1.0', false);
 		wp_deregister_script( 'jquery' );
-		wp_register_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js', false, '1.11.0');
+		wp_register_script( 'jquery', "//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js", false, '1.11.0');
 		wp_enqueue_script( 'jquery' );
 
 		//wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
 
 
 		//wp_enqueue_script( 'scrolltofixed', $jsPath.'vendor/jquery.scrolltofixed.min.js', array('jquery'), '1.0', true);
-		wp_enqueue_script( 'mobileMenu', $jsPath.'vendor/jquery.sidr.js',  array('jquery'), '1.0', false);
-		wp_enqueue_script( 'customSelect', $jsPath.'vendor/jquery.customSelect.min.js',  array('jquery'), '1.0', false);
-		wp_enqueue_script( 'placeHolders', $jsPath.'vendor/jquery.placeholders.js',  array('jquery'), '1.0', false);
-		wp_enqueue_script( 'owl-js', $jsPath.'vendor/owl-carousel/owl.carousel.js',  array('jquery'), '1.0', true);
+		wp_enqueue_script( 'mobileMenu', "$jsPath/vendor/jquery.sidr.js",  array('jquery'), '1.0', false);
+		wp_enqueue_script( 'customSelect', "$jsPath/vendor/jquery.customSelect.min.js",  array('jquery'), '1.0', false);
+		wp_enqueue_script( 'placeHolders', "$jsPath/vendor/jquery.placeholders.js",  array('jquery'), '1.0', false);
+		wp_enqueue_script( 'owl-js', "$jsPath/vendor/owl-carousel/owl.carousel.js",  array('jquery'), '1.0', true);
 
-		wp_enqueue_script( 'siteJS', $jsPath.'site_js.js', array('jquery','mobileMenu','owl-js'), '1.0', true);
+		wp_enqueue_script( 'siteJS', "$jsPath/site_js.js", array('jquery','mobileMenu','owl-js'), '1.0', true);
 
 		wp_enqueue_style( 'g-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic|Open+Sans:300,400italic,400,600,700,800',false, '1', 'all');
-		wp_enqueue_style( 'owl-css-theme', $jsPath.'vendor/owl-carousel/owl.theme.css',array('g-fonts'), '1');
-		wp_enqueue_style( 'owl-css-base', $jsPath.'vendor/owl-carousel/owl.carousel.css',array('owl-css-theme'), '1');
+		wp_enqueue_style( 'owl-css-theme', "$jsPath/vendor/owl-carousel/owl.theme.css",array('g-fonts'), '1');
+		wp_enqueue_style( 'owl-css-base', "$jsPath/vendor/owl-carousel/owl.carousel.css",array('owl-css-theme'), '1');
 		wp_enqueue_style( 'style', $cssPath . 'style.css', array( 'owl-css-base'), '1');
 
 
