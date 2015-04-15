d027516f847829759f6c0b60731bb01be52697e4
ericbdev <ericbdev@gmail.com>
2015-04-15 11:18:20 -0400

    better 404 message handlnig

--- loop/404-message.php
+++ loop/404-message.php
@@ -1,5 +1,8 @@
-<p><?php if (get_lang_active('fr')) { ?>
-	Désolé, nous n'avons pas pu trouver la page que vous cherchiez.
-<?php } else { ?> <!-- English Content -->
-	Sorry, but you've found a page that does not exist, please use the above navigation to find your way back.
-<?php } ?></p>
+<p><?php
+	if (get_lang_active('fr')):
+		echo "Désolé, nous n'avons pas pu trouver la page que vous cherchiez.";
+	else:
+		_ex( "Sorry, but you've found a page that does not exist, please use the above navigation to find your way back.",
+			'Error 404', theme_domain());
+	endif;
+	?></p>
