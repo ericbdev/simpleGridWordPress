180583ba20e677d5ae249ba0158c0fdbb8eb9039
ericbdev <ericbdev@gmail.com>
2015-04-30 11:45:11 -0400

    reording $class and $index

--- includes/base/image.php
+++ includes/base/image.php
@@ -1,7 +1,6 @@
 <?php
 
 
-
 /**
  * Class image
  *
@@ -30,16 +29,16 @@ class image {
 	 * Accepts an image id, and returns back all applicable information.
 	 * Also accepts secondary paramaters to determine size, and other sizes to get of the same image.
 	 *
-	 * @param        $thumbID
+	 * @param                $thumbID
 	 * @param string | array $size
 	 * @return bool | array
 	 */
 	public function get_image($thumbID, $size = 'full') {
 		//todo: return this as non assosiative array
 		$imageSizes = array();
-		if(!is_array($size)){
+		if (!is_array($size)) {
 			$imageSizes[] = $size;
-		}else{
+		} else {
 			$imageSizes = $size;
 		}
 		$thumbOBJ = get_post($thumbID);
@@ -49,9 +48,9 @@ class image {
 			$imageAttr['description'] = $thumbOBJ->post_content;
 			$imageAttr['caption']     = $thumbOBJ->post_excerpt;
 			$imageAttr['title']       = apply_filters('the_title', $thumbOBJ->post_title);
-			$imageAttr['alt_text']         = get_post_meta($thumbOBJ->ID, '_wp_attachment_image_alt', true);
+			$imageAttr['alt_text']    = get_post_meta($thumbOBJ->ID, '_wp_attachment_image_alt', true);
 			$imageAttr['alt']         = ($imageAttr['alt_text'] == '' ? $imageAttr['title'] : $imageAttr['alt_text']);
-			$imageAttr['image'] = array();
+			$imageAttr['image']       = array();
 
 			foreach ($imageSizes as $newSize) {
 				$imageAttr['image'][] = wp_get_attachment_image_src($thumbOBJ->ID, $newSize); // returns an array
@@ -62,13 +61,14 @@ class image {
 			return false;
 		}
 	}
+
 	/**
 	 * @param        $image
 	 * @param int    $index
 	 * @param string $class
 	 * @return string
 	 */
-	public function get_image_html($image, $index = 0, $class = '') {
+	public function get_image_html($image, $class = '', $index = 0) {
 		if ($image):
 			return "<img src='" . $image['image'][$index][0] . "' alt='" . $image['alt'] . "'" . ($class !== '' ? ' class="' . $class . '"' : '') . "/>";
 		else:
@@ -84,16 +84,15 @@ class image {
 	 */
 	public function featured_image($size = 'full') {
 		if (has_post_thumbnail($this->id)) {
-			$thumbID  = get_post_thumbnail_id($this->id);
+			$thumbID = get_post_thumbnail_id($this->id);
 
 			$imageAttr = $this->get_image($thumbID, $size);
 
 			return $imageAttr;
 		} else {
-			return (bool) false;
+			return (bool)false;
 		}
 	}
-
 
 
 	/**
@@ -104,7 +103,7 @@ class image {
 	 *
 	 */
 	public function get_images($size = 'full') {
-		$return    = array();
+		$return = array();
 		$images = get_posts(array(
 			'post_parent'    => $this->id,
 			'post_type'      => 'attachment',
@@ -118,7 +117,7 @@ class image {
 		if ($images !== '') {
 			foreach ($images as $image) {
 				$imgCaption = get_post_meta($image->ID, '_wp_attachment_image_alt', true);
-				$return[] = $this->get_image($image->ID, $size);
+				$return[]   = $this->get_image($image->ID, $size);
 			};
 		}
 		return (empty($return) ? false : $return);
