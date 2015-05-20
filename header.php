6bfe9fe9ff789e0a772fb94f68e45faf7d840838
ericbdev <ericbdev@gmail.com>
2015-05-20 12:10:06 -0400

    hey look at that ,theres a function

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <!doctype html>
 <!--[if lt IE 7 ]>
-<html class="ie ie6 no-js" lang="<?php echo get_lang_active();?>"> <![endif]-->
+<html class="ie ie6 no-js" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 7 ]>
-<html class="ie ie7 no-js" lang="<?php echo get_lang_active();?>"> <![endif]-->
+<html class="ie ie7 no-js" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 8 ]>
-<html class="ie ie8 no-js" lang="<?php echo get_lang_active();?>"> <![endif]-->
+<html class="ie ie8 no-js" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 9 ]>
-<html class="ie ie9 no-js" lang="<?php echo get_lang_active();?>"> <![endif]-->
+<html class="ie ie9 no-js" <?php language_attributes(); ?>> <![endif]-->
 <!--[if gt IE 9]><!-->
 <html class="no-js" lang="<?php echo get_lang_active(); ?>"><!--<![endif]-->
 <!-- the "no-js" class is for Modernizr. -->
