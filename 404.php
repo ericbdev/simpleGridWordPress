1840bc7b5c8b0ed8f2bfe160c1a29e7d65bc885e
ericbdev <ericbdev@gmail.com>
2015-07-09 10:07:18 -0400

    and i wondered why my 404s always derped

--- 404.php
+++ 404.php
@@ -1,8 +1,10 @@
 <?php get_header(); ?>
 	<section class="wrapper main-content default 404">
-		<div class="row">
-			<div class="columns small-12">
-				<?php get_template_part('loop/404-message');?>
+		<div class="inner-wrapper small-padding-left small-padding-right">
+			<div class="row">
+				<div class="columns small-12">
+					<?php get_template_part('loop/404-message');?>
+				</div>
 			</div>
 		</div>
 	</section>
--- index.php
+++ index.php
@@ -17,6 +17,16 @@ if (have_posts()) : while (have_posts()) : the_post(); ?>
 <?php
 endwhile;
 else:
-	get_template_part('404');
+	?>
+	<section class="wrapper main-content default 404">
+		<div class="inner-wrapper small-padding-left small-padding-right">
+			<div class="row">
+				<div class="columns small-12">
+					<?php get_template_part('loop/404-message');?>
+				</div>
+			</div>
+		</div>
+	</section>
+	<?php
 endif;
 get_footer();
